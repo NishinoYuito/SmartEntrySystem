71ed8bf27c27278889269d4b954ac2c4efbfbca4
NishinoYuito <yuito.nishino@arsaga.jp>
2025-02-20 16:54:14 +0900

    ディレクトリ構成の変更

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f8f8",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});

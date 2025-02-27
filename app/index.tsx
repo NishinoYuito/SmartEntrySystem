44e81760fec06c2e77fd17d17760fdd1ca8a8147
yuya.shiraishi <yuya.shiraishi@arsaga.jp>
2025-02-27 15:50:28 +0900

    Supabaseのサンプルコードの実装

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeScreen from "./screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen.sample";
 import "./global.css"
 
 export default function App() {
